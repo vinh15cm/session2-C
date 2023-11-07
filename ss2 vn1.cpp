31fa409c76ef8cf9ac3f5b20e63edd0ee472961f
vinh15cm <147390597+vinh15cm@users.noreply.github.com>
2023-11-07 16:57:46 +0700

    Add files via upload

---
+++ ss2 vn1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+	const float PI=3.14;
+	float edge, radius, length, height;
+	printf ("edge=") ;
+	scanf("%f",&edge);
+	printf("radius=") ;
+	scanf("%f",&radius) ;
+	printf ("length=") 
+	scanf 
+	
+	
+}
